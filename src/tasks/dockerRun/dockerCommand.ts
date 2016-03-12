b10553c32706338ef9ed8195f9bd86229ad5ca6c
Krishna Aditya <krishadi@microsoft.com>
2016-03-12 20:24:30 +0530

    adding certs based auth for remote host

--- src/tasks/dockerRun/dockerCommand.ts
+++ src/tasks/dockerRun/dockerCommand.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../../typings/vsts-task-lib/vsts-task-lib.d.ts" />
 
+import fs = require("fs");
+import path = require("path");
 import tl = require("vsts-task-lib/task");
 import tr = require("vsts-task-lib/toolrunner");
 
@@ -9,6 +11,7 @@ export class DockerCommand {
     public context: string;
     public imageName: string;
     public additionalArguments: string;
+    public dockerConnectionString: string;
     public registryConnetionDetails: tl.EndpointAuthorization;
 
     constructor(commandName: string) {
@@ -17,6 +20,8 @@ export class DockerCommand {
 
     public execSync() {
         var command = this.getBasicCommand();
+
+        this.appendAuth(command);
 
         switch (this.commandName) {
             case "run":
@@ -53,6 +58,32 @@ export class DockerCommand {
         var basicDockerCommand = tl.createToolRunner(dockerPath);
 
         return basicDockerCommand;
+    }
+
+    private appendAuth(command: tr.ToolRunner) {
+        var serverUrl = tl.getEndpointUrl(this.dockerConnectionString, false);
+        var authDetails = tl.getEndpointAuthorization(this.dockerConnectionString, false);
+
+        var dir = path.join("", "certs");
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+
+        var caPath = path.join(dir, "ca.pem");
+        fs.writeFileSync(caPath, authDetails.parameters["username"]);
+
+        var certPath = path.join(dir, "cert.pem");
+        fs.writeFileSync(certPath, authDetails.parameters["password"]);
+
+        var keyPath = path.join(dir, "key.pem");
+        fs.writeFileSync(keyPath, authDetails.parameters["key"]);
+
+        command.arg("--tls");
+        command.arg("--tlscacert='" + caPath + "'");
+        command.arg("--tlscert='" + certPath + "'");
+        command.arg("--tlskey='" + keyPath + "'");
+        command.arg("-H");
+        command.arg(serverUrl);
     }
 
     private appendRunCmdArgs(command: tr.ToolRunner) {
--- src/tasks/dockerRun/dockerRun.ts
+++ src/tasks/dockerRun/dockerRun.ts
@@ -4,11 +4,13 @@ import tl = require("vsts-task-lib/task");
 import * as docker from "./dockerCommand";
 
 export function dockerRun(): void {
+    var dockerConnectionString = tl.getInput("dockerServiceEndpoint", true);
     var imageName = tl.getInput("imageName", true);
     var additionalArgs = tl.getInput("additionalArgs", false);
 
     var cmd = new docker.DockerCommand("run");
     cmd.imageName = imageName;
+    cmd.dockerConnectionString = dockerConnectionString;
     cmd.additionalArguments = additionalArgs;
     cmd.execSync();
 }
