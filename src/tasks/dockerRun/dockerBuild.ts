8ef018375b6705223be988859224c46fec384076
Krishna Aditya <krishadi@microsoft.com>
2016-03-15 17:15:57 +0530

    adding login step to build and run actions too as they need to work on images from private repository

--- src/tasks/dockerRun/dockerBuild.ts
+++ src/tasks/dockerRun/dockerBuild.ts
@@ -7,10 +7,18 @@ import * as docker from "./dockerCommand";
 
 export function dockerBuild(): void {
     var dockerConnectionString = tl.getInput("dockerServiceEndpoint", true);
+    var registryEndpoint = tl.getInput("dockerRegistryServiceEndpoint", true);
     var dockerFile = tl.getInput("dockerFile", true);
     var context = tl.getInput("context", true);
     var imageName = tl.getInput("imageName", true);
     var additionalArgs = tl.getInput("additionalArgs", false);
+
+    var registryConnetionDetails = tl.getEndpointAuthorization(registryEndpoint, true);
+
+    var loginCmd = new docker.DockerCommand("login");
+    loginCmd.dockerConnectionString = dockerConnectionString;
+    loginCmd.registryConnetionDetails = registryConnetionDetails;
+    loginCmd.execSync();
 
     dockerFile = copyDockerFileToContextFolder(dockerFile, context);
 
@@ -21,6 +29,10 @@ export function dockerBuild(): void {
     cmd.imageName = imageName;
     cmd.additionalArguments = additionalArgs;
     cmd.execSync();
+
+    var logoutCmd = new docker.DockerCommand("logout");
+    logoutCmd.dockerConnectionString = dockerConnectionString;
+    logoutCmd.execSync();
 }
 
 function copyDockerFileToContextFolder(dockerFile: string, context: string): string {
--- src/tasks/dockerRun/dockerRun.ts
+++ src/tasks/dockerRun/dockerRun.ts
@@ -5,12 +5,24 @@ import * as docker from "./dockerCommand";
 
 export function dockerRun(): void {
     var dockerConnectionString = tl.getInput("dockerServiceEndpoint", true);
+    var registryEndpoint = tl.getInput("dockerRegistryServiceEndpoint", true);
     var imageName = tl.getInput("imageName", true);
     var additionalArgs = tl.getInput("additionalArgs", false);
+
+    var registryConnetionDetails = tl.getEndpointAuthorization(registryEndpoint, true);
+
+    var loginCmd = new docker.DockerCommand("login");
+    loginCmd.dockerConnectionString = dockerConnectionString;
+    loginCmd.registryConnetionDetails = registryConnetionDetails;
+    loginCmd.execSync();
 
     var cmd = new docker.DockerCommand("run");
     cmd.imageName = imageName;
     cmd.dockerConnectionString = dockerConnectionString;
     cmd.additionalArguments = additionalArgs;
     cmd.execSync();
+
+    var logoutCmd = new docker.DockerCommand("logout");
+    logoutCmd.dockerConnectionString = dockerConnectionString;
+    logoutCmd.execSync();
 }
