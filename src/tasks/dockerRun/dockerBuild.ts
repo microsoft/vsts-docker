c75b92a653468c90bc0934bb49c8219b075a5c53
Krishna Aditya <krishadi@microsoft.com>
2016-03-17 15:27:22 +0530

    adding minimatch pattern support for Dockerfile

--- src/tasks/dockerRun/dockerBuild.ts
+++ src/tasks/dockerRun/dockerBuild.ts
@@ -8,7 +8,7 @@ import * as docker from "./dockerCommand";
 export function dockerBuild(): void {
     var dockerConnectionString = tl.getInput("dockerServiceEndpoint", true);
     var registryEndpoint = tl.getInput("dockerRegistryServiceEndpoint", true);
-    var dockerFile = tl.getInput("dockerFile", true);
+    var dockerFilePattern = tl.getInput("dockerFile", true);
     var context = tl.getInput("context", true);
     var imageName = tl.getInput("imageName", true);
     var additionalArgs = tl.getInput("additionalArgs", false);
@@ -20,6 +20,7 @@ export function dockerBuild(): void {
     loginCmd.registryConnetionDetails = registryConnetionDetails;
     loginCmd.execSync();
 
+    var dockerFile = getDockerFile(dockerFilePattern);
     dockerFile = copyDockerFileToContextFolder(dockerFile, context);
 
     var cmd = new docker.DockerCommand("build");
@@ -44,4 +45,9 @@ function copyDockerFileToContextFolder(dockerFile: string, context: string): str
 
     fs.writeFileSync(target, fs.readFileSync(dockerFile));
     return target;
+}
+
+function getDockerFile(dockerFilePattern: string): string {
+    var dockerFile = tl.globFirst(dockerFilePattern);
+    return dockerFile;
 }
