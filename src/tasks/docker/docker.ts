e5da2bf9faeeec23c880b8b8b0af0fd0249eb726
Devedse <davydavidse@gmail.com>
2017-04-05 09:43:01 +0200

    Cleaning up for pull request

--- src/tasks/docker/docker.ts
+++ src/tasks/docker/docker.ts
@@ -18,18 +18,14 @@ require({
     "Build an image": "./dockerBuild",
     "Push an image": "./dockerPush",
     "Run an image": "./dockerRun",
-    "Run a Docker command": "./dockerCommand",
-    "Clean all images": "./dockerClean"
+    "Run a Docker command": "./dockerCommand"
 }[action]).run(connection)
+/* tslint:enable:no-var-requires */
 .then((result) => {
-    tl._writeLine("Result: " + result);
     const outputVariable = tl.getInput("outputVariableName", false);
-    tl._writeLine("Output variable: ." + outputVariable + ".");
     if (outputVariable !== null) {
         tl.setVariable(outputVariable, result);
         tl._writeLine(`Set ${outputVariable} to: ${result}`);
-    } else {
-        tl._writeLine("Skipping writing of output because no output variable was set. This is normal.");
     }
 })
 .fin(function cleanup() {
